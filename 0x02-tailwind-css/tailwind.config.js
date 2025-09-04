6c23329623843f82a9ab3cfc1ea8a42fe368c2dd
Mohamed Gamal Mahmoud Abdelrehem <112729132+MGM33@users.noreply.github.com>
2025-09-04 17:48:12 +0300

    Tailwind task

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",        // your root HTML file
+    "./src/**/*.{html,js}" // any HTML/JS inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
